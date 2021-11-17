d5f6db3d67c8be4534f0ada99201abb6e6261083
liornagar799 <93442345+liornagar799@users.noreply.github.com>
2021-11-17 13:33:42 +0200

    Update Building.py

--- Ex1/Building.py
+++ Ex1/Building.py
@@ -4,43 +4,27 @@ from Elevators import Elevator
 
 class Building:
 
+    def _init_(self) -> None:
 
+        self._minFloor = 0
+        self._maxFloor = 0
+        self.elevators = []
+        self.size = len(self.elevators)
 
- def __init__(self) -> None:
+    def add(self, elev: Elevator):
+        self.elevators.append(elev)
 
-            self._minFloor=0
-            self._maxFloor=0
-            self.elevators = []
-            self.size=len(self.elevators)
+    def load_json(self, file_name):
 
-
- def add(self, elev: Elevator):
-            self.elevators.append(elev)
-
- def load_json(self, file_name):
-
-    try:
-        with open(file_name, "r+") as f:
+        try:
+            with open(file_name, "r+") as f:
                 build = json.load(f)
                 self._minFloor = build["_minFloor"]
                 self._maxFloor = build["_maxFloor"]
                 self.elevators = build["_elevators"]
-                self.size=len(self.elevators)
-    except IOError as e:
+                self.size = len(self.elevators)
+        except IOError as e:
             print(e)
 
- def _str_(self) -> str:
+    def str(self) -> str:
         return f"minFloor:{self._minFloor}\n maxFloor:{self._maxFloor}\n {self.elevators}"
-
- def getElev(self):
-     return self.elevators
-
-
- def getMaxFloor(self):
-    return self._maxFloor
-
-
- def getMinFloor(self):
-    return self._minFloor
-
-
