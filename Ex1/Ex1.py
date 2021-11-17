51c7c5c8f2551248928650d87295f2c1eb9528d2
liornagar799 <93442345+liornagar799@users.noreply.github.com>
2021-11-17 13:30:32 +0200

    Update Ex1.py

--- Ex1/Ex1.py
+++ Ex1/Ex1.py
@@ -17,7 +17,7 @@ def allocate(call): #return the int of the elevator that be answer on the call
     if numOfElev > 1:#if there is more than one elevator
         if type(call) == 1 or type(call) == -1:#if the type of the call is UP or DOWN
             for i in range(numOfElev):
-                if pos(i) < call.srcFloor: #if the elevator position is low then the src of the call
+                if current_floor[i] < int(call[2]): #if the elevator position is low then the src of the call
                     cnt = calculator(call, i) #calculate the time of answer on the call of i elevator
                     best = min(cnt, best) #check who is the fastest
                     if cnt == best:
@@ -31,19 +31,22 @@ def allocate(call): #return the int of the elevator that be answer on the call
                 ans = i
 
     if flag1 == 1 and flag2 == 0:
-        callsElevators[ans].append(0, call) #adding the call to the elevator we found the best for answer the call
+        callsElevators[ans].append(call) #adding the call to the elevator we found the best for answer the call
         return ans #return the index of the elevator that need to answer the call
     else:
         callsElevators[ans].append(call) #adding the call to the elevator we found the best for answer the call
+    current_floor[ans] = (int(call[3]))
     return ans #return the index of the elevator that need to answer the call
 
 
 def calculator(call, i): #function that calculate the time of  i elevator to go the src of the call c
     elev = b1.elevators
     current = elev[i] #get elevator in index elev
+    if int(call[2]) == current_floor[i]:
+        return 0
     speed = current['_speed']
     OperationTime = current['_stopTime'] + current['_openTime'] + current['_startTime'] + current['_closeTime'] #amount of elevator start time
-    arrivalTime = (abs(int(call[2]) - int(pos(i))))/speed + OperationTime #calculate the time of  i elevator to go the src of the call c
+    arrivalTime = (abs(int(call[2]) - current_floor[i]))/speed + OperationTime #calculate the time of  i elevator to go the src of the call c
     return arrivalTime # the time of  i elevator to go the src of the call c
 
 
@@ -72,45 +75,41 @@ def typeCall(call):
         return 0
 
 
-def pos(i):
-    ans = callsElevators[i][0][2]
-    return ans
-
-
-if __name__ == '_main_':
-    file_ = str(input('Enter Building name'))
-    file_name = str(input('Enter calls name'))
-    out_name = str(input('Enter out name'))
-    # file_=sys.argv[1]
-    # file_name = sys.argv[2]
-    # out_name = sys.argv[3]
+if __name__ == '__main__':
+    file_=sys.argv[1]
+    file_name = sys.argv[2]
+    out_name = sys.argv[3]
     rows = []
-    oly = []
+    cnt = []
     size = 0
+    #reader from csv
     with open(file_name) as file:
         csvreader = csv.reader(file)
         for row in csvreader:
             o = Call(str=row[0], time=float(row[1]), srcFloor=int(row[2]), destFloor=int(row[3]), status=int(row[4]),
                      assign=int(row[5]))
-            oly.append(o)
+            cnt.append(o)
             rows.append(row)
 
-        print(rows)
         b1 = Building()
         b1.load_json(file_)
         Building.add(b1, b1.elevators)
         calls = []
         callsElevators = []
         numOfElev = b1.size
+        # list of calls
         for i in range(0, numOfElev):
             callsElevators.append(calls)
-
+        # list of current_floor of the elevators
+        current_floor = []
+        for t in range(0, numOfElev):
+            current_floor.append(0)
+    # write from csv
     for i in range(0, len(rows)):
         c = rows[i]
         ans = allocate(c)
         rows[i] = Call(str=rows[i][0], time=float(rows[i][1]), srcFloor=int(rows[i][2]), destFloor=int(rows[i][3]),status=int(rows[i][4]), assign=int(ans))
-        #print(rows[i])
+        print(rows[i])
         with open(out_name, 'w') as csv_file:
             for i in rows:
                 csv_file.write(str(i) + '\n')
-
